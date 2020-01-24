1d439d607b36d405b9e0e1080d4cdb3d99834f6e
alukaduolin <763064101@qq.com>
2020-01-24 14:26:56 +0800

    first idea

---
+++ src/com/alukaduolin/first_project/Hello_idea.java
@@ -0,0 +1,24 @@
+package com.alukaduolin.first_project;
+
+public class Hello_idea {
+    public static void main(String[] args) {
+        int a=1;
+        int b=a+1;
+
+        System.out.println(b);
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println();
+        
+        aa();
+        System.out.println("Hello java");
+
+    }
+
+    public static void aa() {
+        System.out.println("aa");
+
+    }
+
+}
